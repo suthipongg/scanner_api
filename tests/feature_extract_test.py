e024576299c2e0bf931bf5facd2440772baced5d
suthipongg <suthipong.mew@gmail.com>
2024-01-15 13:24:10 +0700

    fix buh url

--- tests/feature_extract_test.py
+++ tests/feature_extract_test.py
@@ -10,13 +10,19 @@ client = TestClient(app)
 
 def test_feature_extract_success():
     response = client.post("/cosmenet/scanproduct/v2/onnx", json=JsonUtils.get_json_feature_extract_model(), headers=JsonUtils.header)
-    assert response.status_code == 200
     assert response.json()['message'] == 'success'
+    assert response.status_code == 200
+    
+def test_feature_extract_url():
+    link_image = "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+    response = client.post("/cosmenet/scanproduct/v2/onnx", json={"fullPath": link_image}, headers=JsonUtils.header)
+    assert response.json()['message'] == 'success'
+    assert response.status_code == 200
 
 def test_feature_extract_unauthorized():
     response = client.post("/cosmenet/scanproduct/v2/onnx", json=JsonUtils.get_json_feature_extract_model())
-    assert response.status_code == 401
     assert response.json()['detail'] == "Bearer token missing or unknown"
+    assert response.status_code == 401
 
 if __name__ == "__main__":
     test_feature_extract_success()

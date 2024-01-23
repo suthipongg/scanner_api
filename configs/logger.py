36a19b20eccbfd9a309f9fbc3056ca91bc89a9bf
suthipongg <suthipong.mew@gmail.com>
2024-01-23 10:32:14 +0700

    edit logger

--- configs/logger.py
+++ configs/logger.py
@@ -2,12 +2,12 @@ from datetime import datetime
 import os, logging
 from logging.handlers import TimedRotatingFileHandler
 
-logger = logging.getLogger('uvicorn')
+logger = logging.getLogger("uvicorn")
 logger.setLevel(logging.getLevelName(logging.INFO))
 
 # datetime object containing current date and time
 now = datetime.now()
-dt_string = now.strftime("%Y-%m-%d")
+dt_start_api = now.strftime("%Y-%m-%d")
 
 if not os.path.exists('Logs'):
     try:
@@ -15,10 +15,37 @@ if not os.path.exists('Logs'):
     except Exception as err:
         logging.error(err)
 
+class TimedRotatingFileHandlerCustom(TimedRotatingFileHandler):
+    def __init__(self, filename, when='h', interval=1, backupCount=0, encoding=None, delay=False, utc=False, atTime=None):
+        super().__init__(filename, when, interval, backupCount, encoding, delay, utc, atTime)
+    
+    def rotation_filename(self, default_name):
+        prefix = default_name[len(self.baseFilename + "."):]
+        dirName, baseName = os.path.split(self.baseFilename)
+        new_name = prefix + "_" + baseName
+        return os.path.join(dirName, new_name)
+    
+    def getFilesToDelete(self):
+        dirName, baseName = os.path.split(self.baseFilename)
+        fileNames = os.listdir(dirName)
+        result = []
+        suffix = '_' + baseName
+        slen = len(suffix)
+        for fileName in fileNames:
+            if fileName[-slen:] == suffix:
+                prefix = fileName[:-slen]
+                if self.extMatch.match(prefix):
+                    result.append(os.path.join(dirName, fileName))
+        if len(result) < self.backupCount:
+            result = []
+        else:
+            result.sort()
+            result = result[:len(result) - self.backupCount]
+        return result
+
 def configure_logging():
     # register root logging
     formatter = logging.Formatter("%(asctime)s - %(levelname)s - module:%(module)s - log_line:%(lineno)d - %(message)s")
-    handler = TimedRotatingFileHandler('Logs/' + dt_string + '.log', when="midnight", interval=1, encoding='utf8')
-    handler.suffix = "%Y-%m-%d"
+    handler = TimedRotatingFileHandlerCustom('Logs/' + dt_start_api + '.log', when='MIDNIGHT', encoding='utf8', backupCount=10)
     handler.setFormatter(formatter)
     logger.addHandler(handler)

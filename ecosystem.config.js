dd3a59ae7fc0b57f177c86e46b097fcb1120d70c
suthipongg <suthipong.mew@gmail.com>
2024-06-07 17:01:59 +0700

    edit middleware

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,12 @@
 module.exports = {
     "apps": [{
         "name": "service-python-feature-extractor",
-        "script": "uvicorn app:app --workers 1 --host 0.0.0.0 --port 4446 --log-level critical",
+        "script": "uvicorn app:app --workers 2 --host 0.0.0.0 --port 4446 --log-level critical",
         "instances": "1",
+        "error_file" : "Logs/err.log",
+        "out_file" : "Logs/out.log",
+        "combine_logs": true,
+        "timestamp": true,
+        "log_date_format": "YYYY-MM-DD HH:mm:ss.SSS",
     }]
 }
